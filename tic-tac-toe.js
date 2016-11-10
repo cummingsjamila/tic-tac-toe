9843828e22f8625452c093bd39ccb268b8851ebe
Jamila Cummings <cummingsmeme@gmail.com>
2016-11-10 11:26:42 -0800

    part1
    starting the project

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,3 +1,7 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
+var square1=document.createElement("namespace","square")
+
+function something()
+{alert("Are you sure?")}
