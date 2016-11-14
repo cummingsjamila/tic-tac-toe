2a6cc7612caf07ecdb0014760aa578d11da337b2
Jamila Cummings <cummingsmeme@gmail.com>
2016-11-14 12:15:45 -0800

    part2

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,7 +1,7 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
-var square1=document.createElement("namespace","square")
-
-function something()
-{alert("Are you sure?")}
+function Box1()
+{alert("Are you sure?")
+var canvas=.getElementById("game-board")
+var circle.getElementById("circle1")
