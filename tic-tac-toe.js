90a5f0a572b2e8bfb2c2b842227aca18af5bd3c0
Jamila Cummings <cummingsmeme@gmail.com>
2016-11-17 11:29:05 -0800

    part4

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -2,6 +2,7 @@
 // You can use the namespace variable that's defined here.
 var namespace = "http://www.w3.org/2000/svg"
 var canvas=document.getElementById("game-board")
+
 currentShape="circle"
 function shapes() {
   if(currentShape=="rect"){
@@ -10,7 +11,8 @@ circle.setAttribute("cx", 150)
 circle.setAttribute("cy", 110)
 circle.setAttribute("r", 65)
 circle.setAttribute("fill","white")
-circle.setAttribute("stroke-color", "black")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
 currentShape="circle" }
 else{
@@ -20,10 +22,204 @@ else{
   rect.setAttribute("width", 120)
   rect.setAttribute("height", 60)
   rect.setAttribute("fill", "white")
-  rect.setAttribute("stroke-color", "black")
+  rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"
-
+  currentShape="rect"}
 }
+function shapes2() {
+  if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 420)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+  var rect=document.createElementNS(namespace,"rect")
+  rect.setAttribute("x", 350)
+  rect.setAttribute("y", 100)
+  rect.setAttribute("width", 120)
+  rect.setAttribute("height", 60)
+  rect.setAttribute("fill", "white")
+  rect.setAttribute("stroke", "black")
+  rect.setAttribute("stroke-width", 8)
+  canvas.appendChild(rect)
+  currentShape="rect"}
+}
+
+function shapes3() {
+  if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+  var rect=document.createElementNS(namespace,"rect")
+  rect.setAttribute("x", 50)
+  rect.setAttribute("y", 100)
+  rect.setAttribute("width", 120)
+  rect.setAttribute("height", 60)
+  rect.setAttribute("fill", "white")
+  rect.setAttribute("stroke", "black")
+  rect.setAttribute("stroke-width", 8)
+  canvas.appendChild(rect)
+  currentShape="rect"}
+}
+
+function shapes4()
+if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+var rect=document.createElementNS(namespace,"rect")
+rect.setAttribute("x", 50)
+rect.setAttribute("y", 100)
+rect.setAttribute("width", 120)
+rect.setAttribute("height", 60)
+rect.setAttribute("fill", "white")
+rect.setAttribute("stroke", "black")
+rect.setAttribute("stroke-width", 8)
+canvas.appendChild(rect)
+currentShape="rect"}
+}
+
+function shapes5()
+if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+var rect=document.createElementNS(namespace,"rect")
+rect.setAttribute("x", 50)
+rect.setAttribute("y", 100)
+rect.setAttribute("width", 120)
+rect.setAttribute("height", 60)
+rect.setAttribute("fill", "white")
+rect.setAttribute("stroke", "black")
+rect.setAttribute("stroke-width", 8)
+canvas.appendChild(rect)
+currentShape="rect"}
+}
+function shapes6()
+if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+  var rect=document.createElementNS(namespace,"rect")
+  rect.setAttribute("x", 50)
+  rect.setAttribute("y", 100)
+  rect.setAttribute("width", 120)
+  rect.setAttribute("height", 60)
+  rect.setAttribute("fill", "white")
+  rect.setAttribute("stroke", "black")
+  rect.setAttribute("stroke-width", 8)
+  canvas.appendChild(rect)
+  currentShape="rect"}
+}
+function shapes7() {
+}
+if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+  var rect=document.createElementNS(namespace,"rect")
+  rect.setAttribute("x", 50)
+  rect.setAttribute("y", 100)
+  rect.setAttribute("width", 120)
+  rect.setAttribute("height", 60)
+  rect.setAttribute("fill", "white")
+  rect.setAttribute("stroke", "black")
+  rect.setAttribute("stroke-width", 8)
+  canvas.appendChild(rect)
+  currentShape="rect"}
+}
+function shapes8 ()
+if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+  var rect=document.createElementNS(namespace,"rect")
+  rect.setAttribute("x", 50)
+  rect.setAttribute("y", 100)
+  rect.setAttribute("width", 120)
+  rect.setAttribute("height", 60)
+  rect.setAttribute("fill", "white")
+  rect.setAttribute("stroke", "black")
+  rect.setAttribute("stroke-width", 8)
+  canvas.appendChild(rect)
+  currentShape="rect"}
+}
+function shapes9()
+if(currentShape=="rect"){
+var circle=document.createElementNS(namespace,"circle")
+circle.setAttribute("cx", 150)
+circle.setAttribute("cy", 110)
+circle.setAttribute("r", 65)
+circle.setAttribute("fill","white")
+circle.setAttribute("stroke", "black")
+circle.setAttribute("stroke-width", 8)
+canvas.appendChild(circle)
+currentShape="circle" }
+else{
+  var rect=document.createElementNS(namespace,"rect")
+  rect.setAttribute("x", 50)
+  rect.setAttribute("y", 100)
+  rect.setAttribute("width", 120)
+  rect.setAttribute("height", 60)
+  rect.setAttribute("fill", "white")
+  rect.setAttribute("stroke", "black")
+  rect.setAttribute("stroke-width", 8)
+  canvas.appendChild(rect)
+  currentShape="rect"}
+}
+
+
+
+
+
+
+
 }
