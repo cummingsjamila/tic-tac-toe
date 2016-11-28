f6bce1b9a161e3f5428d615499b96fdf25a9ef13
Jamila Cummings <cummingsmeme@gmail.com>
2016-11-28 12:16:19 -0800

    part6

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,11 +1,14 @@
 // Write all your JavaScript code in this file!
 // You can use the namespace variable that's defined here.
+function reset()
+{location.reload();
+}
 var namespace = "http://www.w3.org/2000/svg"
 var canvas=document.getElementById("game-board")
-var buttonClicked=8
+var buttonClicked1=8
 currentShape="circle"
 function shapes() {
-  if(buttonClicked== 8){
+  if(buttonClicked1 == 8){
   if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 150)
@@ -16,7 +19,7 @@ circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
 currentShape="circle"
-buttonClicked==9 }
+buttonClicked1=9 }
 else{
   var rect=document.createElementNS(namespace,"rect")
   rect.setAttribute("x", 50)
@@ -27,11 +30,14 @@ else{
   rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"}
+  currentShape="rect"
+  buttonClicked1=9
+  }
 }
 }
+var buttonClicked2=8
 function shapes2() {
-  if(buttonClicked== 8){
+  if(buttonClicked2== 8){
   if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 420)
@@ -41,7 +47,8 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked2=9 }
 else{
   var rect=document.createElementNS(namespace,"rect")
   rect.setAttribute("x", 350)
@@ -52,11 +59,13 @@ else{
   rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"}
+  currentShape="rect"
+buttonclicked2=9}
 }
 }
+var buttonClicked3=8
 function shapes3() {
-  if(buttonClicked== 8){
+  if(buttonClicked3== 8){
   if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 620)
@@ -66,7 +75,8 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked3=9  }
 else{
   var rect=document.createElementNS(namespace,"rect")
   rect.setAttribute("x", 580)
@@ -77,11 +87,13 @@ else{
   rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"}
+  currentShape="rect"
+buttonClicked3=9}
 }
 }
+var buttonClicked4=8
 function shapes4(){
-  if(buttonClicked== 8){
+  if(buttonClicked4== 8){
 if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 140)
@@ -91,7 +103,8 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked4=9  }
 else{
 var rect=document.createElementNS(namespace,"rect")
 rect.setAttribute("x", 50)
@@ -102,11 +115,13 @@ rect.setAttribute("fill", "white")
 rect.setAttribute("stroke", "black")
 rect.setAttribute("stroke-width", 8)
 canvas.appendChild(rect)
-currentShape="rect"}
+currentShape="rect"
+buttonClicked4=9}
 }
 }
+var buttonClicked5=8
 function shapes5(){
-  if(buttonClicked== 8){
+  if(buttonClicked5== 8){
 if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 420)
@@ -116,7 +131,8 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked5=9  }
 else{
 var rect=document.createElementNS(namespace,"rect")
 rect.setAttribute("x", 390)
@@ -127,11 +143,13 @@ rect.setAttribute("fill", "white")
 rect.setAttribute("stroke", "black")
 rect.setAttribute("stroke-width", 8)
 canvas.appendChild(rect)
-currentShape="rect"}
+currentShape="rect"
+buttonClicked5=9}
 }
 }
+var buttonClicked6=8
 function shapes6(){
-  if(buttonClicked== 8){
+  if(buttonClicked6== 8){
 if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 610)
@@ -141,7 +159,8 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked6=9  }
 else{
   var rect=document.createElementNS(namespace,"rect")
   rect.setAttribute("x", 580)
@@ -152,11 +171,13 @@ else{
   rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"}
+  currentShape="rect"
+buttonClicked6=9}
 }
 }
+var buttonClicked7=8
 function shapes7() {
-  if(buttonClicked== 8){
+  if(buttonClicked7== 8){
 if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 150)
@@ -166,10 +187,11 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked7=9 }
 else{
   var rect=document.createElementNS(namespace,"rect")
-  rect.setAttribute("x", 50)
+  rect.setAttribute("x", 150)
   rect.setAttribute("y", 600)
   rect.setAttribute("width", 120)
   rect.setAttribute("height", 60)
@@ -177,11 +199,13 @@ else{
   rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"}
+  currentShape="rect"
+buttonClicked7=9}
 }
 }
+var buttonClicked8=8
 function shapes8 () {
-  if(buttonClicked==8){
+  if(buttonClicked8==8){
 if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
 circle.setAttribute("cx", 450)
@@ -191,7 +215,8 @@ circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked8=9  }
 else{
   var rect=document.createElementNS(namespace,"rect")
   rect.setAttribute("x", 360)
@@ -205,18 +230,20 @@ else{
   currentShape="rect"}
 }
 }
+var buttonClicked9=8
 function shapes9() {
-  if (buttonClicked == 8){
+  if (buttonClicked9 == 8){
 if(currentShape=="rect"){
 var circle=document.createElementNS(namespace,"circle")
-circle.setAttribute("cx", 590)
+circle.setAttribute("cx", 610)
 circle.setAttribute("cy", 540)
 circle.setAttribute("r", 65)
 circle.setAttribute("fill","white")
 circle.setAttribute("stroke", "black")
 circle.setAttribute("stroke-width", 8)
 canvas.appendChild(circle)
-currentShape="circle" }
+currentShape="circle"
+buttonClicked9=9 }
 else{
   var rect=document.createElementNS(namespace,"rect")
   rect.setAttribute("x", 560)
@@ -227,6 +254,7 @@ else{
   rect.setAttribute("stroke", "black")
   rect.setAttribute("stroke-width", 8)
   canvas.appendChild(rect)
-  currentShape="rect"}
+  currentShape="rect"
+buttonClicked9=9}
 }
 }
